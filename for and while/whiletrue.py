06eaddfd8f632d45c6209e54e806bfbdab082ffa
Humberto Itiró Morimitsu Filho <126424817+Betoitiro@users.noreply.github.com>
2023-04-03 11:39:50 -0300

    while true

---
+++ for and while/whiletrue.py
@@ -0,0 +1,16 @@
+#usando while como condição verdadeira
+#vai ler uma lista  somar com o numero digitado pelo usuario
+
+num =[] #comando que vai listar os valores digitados em n
+s = 2 #soma feita no final
+
+while True:
+    n = input("Digite um numero(ou 'fim' para encerrar): ")
+    if n == 'fim': #condição para que quando chegue ao fim os numeros parem se ser acrecentados na lista
+        break
+    num.append(int(n))  #comando que acrecenta n dentro da lista de num
+
+for n in num: #loop para que n recebesse os elementos da lista a cada interação do loop
+    s +=n #soma dos elementos
+
+print(f"A soma dos numeros é {s}")
