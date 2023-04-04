da8dd1fc45de0f3848d9fe501e7c0bddaff32ad5
Humberto Itiró Morimitsu Filho <126424817+Betoitiro@users.noreply.github.com>
2023-04-03 21:11:17 -0300

    atividade de sala

---
+++ aula 3/exercicio de sala 2.py
@@ -0,0 +1,19 @@
+
+# num = int(input("digite um numero: "))
+
+# for i in range(1):
+#     if num % 2 == 0:
+#         print("par")
+#     else:
+#         print("impar")
+
+# executar = 'sim'
+# while executar == 'sim':
+#     num = int(input("digite um numero: "))
+#     if num % 2 == 0:
+#         print("o numero é par")
+#     else:
+#         print("o numero é impar")
+#     executar = str.lower
+#     break
+
