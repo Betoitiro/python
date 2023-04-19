e86dca32a5ffb97167b891752b7a5e483e137431
Humberto Itiró Morimitsu Filho <126424817+Betoitiro@users.noreply.github.com>
2023-04-19 19:17:06 -0300

    teste

--- ESTUDOS/ex05/teste.py
+++ ESTUDOS/ex05/teste.py
@@ -1,19 +1,13 @@
-def maior_preço(list):
-    maior = None
-    for item in list:
-        total = item['preço'] * item['quantidade']
-        item['total'] = total # adiciona a chave 'total' com o valor da multiplicação
-        if maior is None or total > maior['preço'] * maior['quantidade']:
-            maior = item
-    return maior
+def palavra_frequente(list):
+    contar = {} #serve para usar de chave para string no sistema
+    for palavra in list:
+        if palavra in contar:
+            contar[palavra] +=1
+        else:
+            contar[palavra] =1
 
-produto = [
-    {'produto': 'maça', 'preço': 2.5, 'quantidade': 10},
-    {'produto': 'pêra', 'preço': 3.0, 'quantidade': 15},
-    {'produto': 'maracuja', 'preço': 4.0, 'quantidade': 12}
-]
+    return max(contar, key=contar.get) #atrubui a variavel contar a chave
 
-maior = maior_preço(produto)
-
-print("Produto com maior valor total:", maior)
-print("Valor total:", maior['total']) # mostra o valor total do produto com maior valor
+list = ['branco', 'vermelho', 'vermelho', 'azul', 'verde', 'amarelo', ' vermelho']
+palavra = palavra_frequente(list)
+print(palavra)
