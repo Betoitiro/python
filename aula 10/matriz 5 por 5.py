b07112b68b6facaeaf1aab5f3df486c01e88d46d
Humberto Itiró Morimitsu Filho <126424817+Betoitiro@users.noreply.github.com>
2023-04-24 20:11:25 -0300

    atividade de sala

--- aula 10/matriz 5 por 5.py
+++ aula 10/matriz 5 por 5.py
@@ -1,4 +1,4 @@
-list = [
+list1 = [
     [1,2,3,2,58],
     [8,9,6,3,6],
     [8,36,36,39,35],
@@ -6,8 +6,17 @@ list = [
     [47,58,69,32,25]
 ]
 
-print(list[0])
-print(list[1])
-print(list[2])
-print(list[3])
-print(list[4])
+
+for z in list1:
+    print(z)
+
+print()
+
+for lista in list1:
+    for item in lista:
+        if (item %2) ==0:
+            print("esse intem é par {}".format(item))
+        elif item ==5:
+            print("esse numero é 5 {}".format(item))
+        elif (item %2) != 0:
+            print("este numero é impar {} ".format(item))
