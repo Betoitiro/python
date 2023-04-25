1c182f744e9c794133886116c43e971ff5c4812e
Humberto Itiró Morimitsu Filho <126424817+Betoitiro@users.noreply.github.com>
2023-04-24 21:02:47 -0300

    atividade de tabela 10x 100 em matriz

---
+++ aula 10/randon_at2.py
@@ -0,0 +1,12 @@
+import random
+
+n = int(input("Digite a dimensão n da matriz: "))
+m = int(input("Digite a diminensão m da matriz: "))
+matriz = []
+
+for i in range(n):
+    linha = []
+    for j in range(m):
+        linha.append(random.randint(10,100))
+    matriz.append(linha)
+    print(matriz)
